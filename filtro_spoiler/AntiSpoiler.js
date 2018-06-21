a61c19748d37d24c78eef12a85585067f4bdafb6
João Cabral <jv_cabral@outlook.com>
2018-06-21 15:34:36 -0300

    git checkout

--- filtro_spoiler/AntiSpoiler.js
+++ filtro_spoiler/AntiSpoiler.js
@@ -1,18 +1,29 @@
 $(function () {
     let i = 0;
-   let spoilers = ["café", "Café", "cafe"];
-   let cafe = "café"
-   $.each(spoilers, function(index, value){
-        $('a, p, span, h1, h2, h3, h4, h5, h6').each(function () {
+    var herois;
+   let cafe = ['Café', 'café', 'CAFE'];
+    $.ajax({
+    
+        url: 'https://gateway.marvel.com/v1/public/events/314/characters?&ts=1&apikey=48985e65e2babdeac044a1ad73d5b299&hash=b4e87ab657e8c6aedeed9c93780c917a',
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+          let herois = result.data.results;
+         //result.data.results.name
+           /*$.each(resultados, function(index, value){
+                herois[index] = value.name;
+            }); */
 
-           
+            
+   $.each(herois, function(index, value){
+        $('a, p, span, h1, h2, h3, h4, h5, h6').each(function () {
                // console.log(value);
-                if ($(this).is(':contains("'+value+'")')) {
+                if ($(this).is(':contains("'+value.name+'")')) {
                     
                     let posicao = $(this).position();
     
                     $(this).append('<div data-clone="sim" id="novo' + i + '" style="position:absolute; width:' + $(this).width() + 'px;height:' + $(this).height() + 'px">SPOILERS</div>');
-                    console.log($(this).width());
+                   // console.log($(this).width());
                     $('#novo' + i).animate({
                         top: posicao.top,
                         left: posicao.left,
@@ -28,6 +39,13 @@ $(function () {
             });            
 
         });
+
+
+
+            
+        }
+    });
+    
         $(document).on('mouseover', '[data-clone="sim"]', function () {
 
 
